cd95bbf09d2504f5e097ab4c36a46167f9c25e4c
kuijunming93 <kuijunming93@gmail.com>
2021-12-05 14:20:06 +0800

    Add background image

--- reactApp/gameconnect/src/App.js
+++ reactApp/gameconnect/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import { Component } from 'react';
 import Main from "./components/mainComponent";
+import bg from './assets/images/earthWallpaper.jpg';
+
 
 class App extends Component{
 
   render(){
     return(
-      <div className="App">
+      <div className="App" style={{ backgroundImage: {bg} }}>
         <Main />
       </div>
     );

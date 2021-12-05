9dfeb99d9f44e6655d5137e903bdd3732544c321
kuijunming93 <kuijunming93@gmail.com>
2021-12-05 20:09:11 +0800

    Updated footer font

--- reactApp/gameconnect/src/components/footerComponent.js
+++ reactApp/gameconnect/src/components/footerComponent.js
@@ -3,8 +3,8 @@ import {Spinner} from 'react-bootstrap';
 
 const Footer = () => {
     return(
-        <div className="footer container row mt-3 mb-3 row d-flex justify-content-center">
-            <h3 style={{fontSize:"4vw"}}> Stay tuned for more to come..</h3>
+        <div className="container row mt-3 mb-3 row d-flex justify-content-center">
+            <span style={{fontSize:"4vw", color:"white"}}><light>Stay tuned for more to come..</light></span>
             <div className="row d-flex justify-content-center">
                 <Spinner style={{animationDelay:"-0.5s"}} animation="grow" variant="primary" />
                 <Spinner style={{animationDelay:"-0.4s"}} animation="grow" variant="secondary" />

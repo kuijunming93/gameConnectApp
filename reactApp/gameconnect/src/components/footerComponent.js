ab12bd015f06610e33000b2eea40e2148bcb93a9
kuijunming93 <kuijunming93@gmail.com>
2021-12-05 23:36:36 +0800

    Fixed footer centering

--- reactApp/gameconnect/src/components/footerComponent.js
+++ reactApp/gameconnect/src/components/footerComponent.js
@@ -3,9 +3,11 @@ import {Spinner} from 'react-bootstrap';
 
 const Footer = () => {
     return(
-        <div className="container row mt-3 mb-3 row d-flex justify-content-center">
+        <div className="mt-3 mb-3 row d-flex justify-content-center">
+            <div className="">
             <span style={{fontSize:"4vw", color:"white"}}><light>Stay tuned for more to come..</light></span>
-            <div className="row d-flex justify-content-center">
+            </div>
+            <div className="d-flex justify-content-center mt-2 mb-2">
                 <Spinner style={{animationDelay:"-0.5s"}} animation="grow" variant="primary" />
                 <Spinner style={{animationDelay:"-0.4s"}} animation="grow" variant="secondary" />
                 <Spinner style={{animationDelay:"-0.3s"}} animation="grow" variant="success" />

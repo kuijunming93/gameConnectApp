0c12f3c4b5d1dd68ad582d68efadbbf417f4f65e
kuijunming93 <kuijunming93@gmail.com>
2021-12-17 19:38:42 +0800

    Completed week 1 assignment

--- courseraBackend/node-express/index.js
+++ courseraBackend/node-express/index.js
@@ -9,12 +9,23 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/'));
 app.use(bodyParser.json());
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+});
 
 const server = http.createServer(app);
 

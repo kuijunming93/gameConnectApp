66578bd77c445e70b6dbea26013493b392521103
kuijunming93 <kuijunming93@gmail.com>
2021-12-19 13:35:27 +0800

    Mongoose Part 3 - added more to schema

--- courseraBackend/node-mongoose/index.js
+++ courseraBackend/node-mongoose/index.js
@@ -9,18 +9,33 @@ connect.then((db) => {
 
     console.log('Connected correctly to server');
 
-    // creating directly on the mongoose schema using mongoose command
     Dishes.create({
-        name: 'Uthapizza',
-        description: 'Test'
+        name: 'Uthappizza',
+        description: 'test'
     })
     .then((dish) => {
         console.log(dish);
-        
-        return Dishes.find({}).exec();
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Updated test'}
+        },{ 
+            new: true 
+        })
+        .exec();
     })
-    .then((dishes) => {
-        console.log(dishes);
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 5,
+            comment: 'I\'m getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        });
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
 
         return Dishes.remove({});
     })
@@ -30,6 +45,28 @@ connect.then((db) => {
     .catch((err) => {
         console.log(err);
     });
+
+    // creating directly on the mongoose schema using mongoose command
+    // Dishes.create({
+    //     name: 'Uthapizza',
+    //     description: 'Test'
+    // })
+    // .then((dish) => {
+    //     console.log(dish);
+        
+    //     return Dishes.find({}).exec();
+    // })
+    // .then((dishes) => {
+    //     console.log(dishes);
+
+    //     return Dishes.remove({});
+    // })
+    // .then(() => {
+    //     return mongoose.connection.close();
+    // })
+    // .catch((err) => {
+    //     console.log(err);
+    // });
 
     // creating a var and saving it under mongoose schema
     // var newDish = Dishes({
